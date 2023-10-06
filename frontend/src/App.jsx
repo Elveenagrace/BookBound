340ca69ded9783125f08a734fcd8ad45c38c4289
Elveenagrace <elveenagrace@gmail.com>
2023-10-06 17:33:07 +0530

    evening(day-2)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-//import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import CreateBooks from './pages/CreateBooks';
@@ -8,12 +9,12 @@ import DeleteBooks from './pages/DeleteBooks';
 const App = () => {
   return (
     <Routes>
-  <Route path='/' element={<Home />} />
-  <Route path='/books/create' element={<CreateBooks />} />
-  <Route path='/books/details/:id' element={<ShowBook />} />
-  <Route path='/books/edit/:id' element={<EditBooks/>} />
-  <Route path='/books/delete/:id' element={<DeleteBooks/>} />
-</Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBooks/>} />
+      <Route path='/books/delete/:id' element={<DeleteBooks/>} />
+   </Routes>
     
   );
 };
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const spinner = () => {
+  return (
+    <div className='animate-ping-w16 h-16 m-8 rounded-full bg-sky-600' >spinner</div>
+  )
+}
+
+export default spinner
